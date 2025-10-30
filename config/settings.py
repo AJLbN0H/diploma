5d68874da2b51258ce8b01303b5f3b390f1ae36e
alexander vagin <sashavagin1987@gmail.com>
2025-10-30 21:45:33 +1000

    - в INSTALLED_APPS добавлены приложения users и materials - AUTH_USER_MODEL изменен на users.User (аутентификация происходит через модель User)

--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'users',
+    'materials',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +129,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
