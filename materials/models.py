e5abd4bf7bd6f42b339a5d97b6085e872c92111f
alexander vagin <sashavagin1987@gmail.com>
2025-10-30 21:46:04 +1000

    - созданы модели Section и Material

--- materials/models.py
+++ materials/models.py
@@ -1,3 +1,69 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+
+
+class Section(models.Model):
+    name = models.CharField(
+        max_length=200, verbose_name="Название раздела", help_text="Введите название раздела"
+    )
+    description = models.TextField(
+        verbose_name="Описание раздела",
+        blank=True,
+        null=True,
+        help_text="Введите описание раздела",
+    )
+    owner = models.ForeignKey(
+        User,
+        verbose_name="Владелец раздела",
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Раздед"
+        verbose_name_plural = "Разделы"
+
+
+class Material(models.Model):
+    name = models.CharField(
+        max_length=200, verbose_name="Название материала", help_text="Введите название материала"
+    )
+    description = models.TextField(
+        verbose_name="Описание материала",
+        blank=True,
+        null=True,
+        help_text="Введите описание материала",
+    )
+    course = models.ForeignKey(
+        Section,
+        verbose_name="Курс",
+        on_delete=models.SET_NULL,
+        help_text="Выберите курс",
+        null=True,
+        blank=True,
+    )
+    owner = models.ForeignKey(
+        User,
+        verbose_name="Владелец материала",
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Материал"
+        verbose_name_plural = "Материалы"
+
+
