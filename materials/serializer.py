51d2e9a770e9a5d7cc794b1f4a2a46761c886320
alexander vagin <sashavagin1987@gmail.com>
2025-10-31 21:51:42 +1000

    - создан SectionSerializer

--- materials/serializer.py
+++ materials/serializer.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from materials.models import Material
+from materials.models import Material, Section
 
 
 class MaterialSerializer(serializers.ModelSerializer):
@@ -8,4 +8,12 @@ class MaterialSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Material
+        fields = "__all__"
+
+
+class SectionSerializer(serializers.ModelSerializer):
+    """Сериалайзер модели Section."""
+
+    class Meta:
+        model = Section
         fields = "__all__"
