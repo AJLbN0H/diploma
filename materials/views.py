c968024de36d5a4e9f4f9313b0222f93e32ee105
alexander vagin <sashavagin1987@gmail.com>
2025-10-31 21:49:31 +1000

    - реализован полный CRUD для модели Material

--- materials/views.py
+++ materials/views.py
@@ -1,11 +1,39 @@
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, ListAPIView, DestroyAPIView, UpdateAPIView
 
 from materials.models import Material
 from materials.serializer import MaterialSerializer
 
 
-class MaterialCreateApiView(CreateAPIView):
+class MaterialCreateAPIView(CreateAPIView):
     """Generic создания материалов."""
+
+    queryset = Material.objects.all()
+    serializer_class = MaterialSerializer
+
+
+class MaterialListAPIView(ListAPIView):
+    """Generic списка материалов."""
+
+    queryset = Material.objects.all()
+    serializer_class = MaterialSerializer
+
+
+class MaterialRetrieveAPIView(ListAPIView):
+    """Generic просмотра подробной информации о материале."""
+
+    queryset = Material.objects.all()
+    serializer_class = MaterialSerializer
+
+
+class MaterialUpdateAPIView(UpdateAPIView):
+    """Generic обновления ирформации материалов."""
+
+    queryset = Material.objects.all()
+    serializer_class = MaterialSerializer
+
+
+class MaterialDestroyAPIView(DestroyAPIView):
+    """Generic удаления материалов."""
 
     queryset = Material.objects.all()
     serializer_class = MaterialSerializer
