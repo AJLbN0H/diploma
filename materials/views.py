d191cffcecc1b4a1e84297e65239e172b4ecc60b
alexander vagin <sashavagin1987@gmail.com>
2025-10-31 23:50:51 +1000

    - контроллеры с generic измены на viewset - пути изменены на ViewSet

--- materials/views.py
+++ materials/views.py
@@ -1,79 +1,19 @@
-from rest_framework.generics import (
-    CreateAPIView,
-    ListAPIView,
-    DestroyAPIView,
-    UpdateAPIView,
-)
+from rest_framework.viewsets import ViewSet
 
 from materials.models import Material, Section
 from materials.serializer import MaterialSerializer, SectionSerializer
 
 
-class SectionCreateAPIView(CreateAPIView):
-    """Generic создания раздела."""
+class SectionViewSet(ViewSet):
+    """ViewSet модели Section."""
 
     queryset = Section.objects.all()
     serializer_class = SectionSerializer
 
 
-class SectionListAPIView(ListAPIView):
-    """Generic списка разделов."""
-
-    queryset = Section.objects.all()
-    serializer_class = SectionSerializer
-
-
-class SectionRetrieveAPIView(ListAPIView):
-    """Generic просмотра подробной информации о разделе."""
-
-    queryset = Section.objects.all()
-    serializer_class = SectionSerializer
-
-
-class SectionUpdateAPIView(UpdateAPIView):
-    """Generic обновления ирформации раздела."""
-
-    queryset = Section.objects.all()
-    serializer_class = SectionSerializer
-
-
-class SectionDestroyAPIView(DestroyAPIView):
-    """Generic удаления раздела."""
-
-    queryset = Section.objects.all()
-    serializer_class = SectionSerializer
-
-
-class MaterialCreateAPIView(CreateAPIView):
-    """Generic создания материала."""
+class MaterialViewSet(ViewSet):
+    """ViewSet модели Material."""
 
     queryset = Material.objects.all()
     serializer_class = MaterialSerializer
 
-
-class MaterialListAPIView(ListAPIView):
-    """Generic списка материалов."""
-
-    queryset = Material.objects.all()
-    serializer_class = MaterialSerializer
-
-
-class MaterialRetrieveAPIView(ListAPIView):
-    """Generic просмотра подробной информации о материале."""
-
-    queryset = Material.objects.all()
-    serializer_class = MaterialSerializer
-
-
-class MaterialUpdateAPIView(UpdateAPIView):
-    """Generic обновления ирформации материала."""
-
-    queryset = Material.objects.all()
-    serializer_class = MaterialSerializer
-
-
-class MaterialDestroyAPIView(DestroyAPIView):
-    """Generic удаления материала."""
-
-    queryset = Material.objects.all()
-    serializer_class = MaterialSerializer
