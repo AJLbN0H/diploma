97a667143e7d8344072d63e69a5c88af42b6990a
alexander vagin <sashavagin1987@gmail.com>
2025-10-31 21:39:18 +1000

    - создан MaterialSerializer

---
+++ materials/serializer.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+
+from materials.models import Material
+
+
+class MaterialSerializer(serializers.ModelSerializer):
+    """Сериалайзер модели Material."""
+
+    class Meta:
+        model = Material
+        fields = '__all__'
