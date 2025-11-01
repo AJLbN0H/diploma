5b6e484eff36d006d4d8d8ff746e5d7fd5505625
alexander vagin <sashavagin1987@gmail.com>
2025-11-01 22:46:03 +1000

    - добавлено поле role в модель User

--- users/models.py
+++ users/models.py
@@ -5,30 +5,34 @@ from django.db import models
 class User(AbstractUser):
     username = None
 
+    ROLE = [
+        ('admin', 'Администратор'),
+        ('teacher', 'Преподователь'),
+        ('student', 'Студент')
+    ]
+
     email = models.EmailField(
-        unique=True, verbose_name="Почта", help_text="Уажите почту"
+        unique=True, verbose_name="Почта"
     )
     avatar = models.ImageField(
         upload_to="users/",
         verbose_name="Аватар",
         blank=True,
         null=True,
-        help_text="Загрузите свой аватар",
     )
     phone = models.CharField(
         max_length=35,
         verbose_name="Номер телефона",
         blank=True,
         null=True,
-        help_text="Введите номер телефона",
     )
     city = models.CharField(
         max_length=50,
         verbose_name="Город",
         blank=True,
         null=True,
-        help_text="Введите город в котором вы живете",
     )
+    role = models.CharField(max_length=7, verbose_name='Роль', choices=ROLE, default='student')
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
