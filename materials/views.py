d6affa7103cfcb425cf9590d16f401838d35ddf6
alexander vagin <sashavagin1987@gmail.com>
2025-11-02 04:10:45 +1000

    - установлены права доступа для SectionViewSet и MaterialViewSet

--- materials/views.py
+++ materials/views.py
@@ -1,6 +1,9 @@
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 from materials.models import Material, Section
+from materials.permission import IsOwner, IsStudent, IsTeacher, IsAdmin, IsAdminOrTeacherOwner, IsAdminOrTeacher, \
+    IsAdminOrTeacherOrStudent
 from materials.serializer import MaterialSerializer, SectionSerializer
 
 
@@ -10,9 +13,44 @@ class SectionViewSet(ModelViewSet):
     queryset = Section.objects.all()
     serializer_class = SectionSerializer
 
+    def get_permissions(self):
+        """Метод проверки прав доступа."""
+
+        if self.action == "create":
+            self.permission_classes = [IsAdminOrTeacher]
+        if self.action == "list":
+            self.permission_classes = [IsAdminOrTeacher]
+        elif self.action in ["partial_update", "update", "retrieve"]:
+            self.permission_classes = [IsAdminOrTeacherOwner]
+        if self.action == "destroy":
+            self.permission_classes = [IsAdminOrTeacherOwner]
+        return super().get_permissions()
+
+    def perform_create(self, serializer):
+        """Метод переопределяющий при создании урока поле owner на текущего авторизованного пользователя."""
+
+        serializer.save(owner=self.request.user)
 
 class MaterialViewSet(ModelViewSet):
     """ViewSet модели Material."""
 
     queryset = Material.objects.all()
     serializer_class = MaterialSerializer
+
+    def get_permissions(self):
+        """Метод проверки прав доступа."""
+
+        if self.action == "create":
+            self.permission_classes = [IsAdminOrTeacher]
+        if self.action == "list":
+            self.permission_classes = [IsAdminOrTeacherOrStudent]
+        elif self.action in ["partial_update", "update", "retrieve"]:
+            self.permission_classes = [IsAdminOrTeacherOwner]
+        elif self.action == "destroy":
+            self.permission_classes = [IsAdminOrTeacherOwner]
+        return super().get_permissions()
+
+    def perform_create(self, serializer):
+        """Метод переопределяющий при создании урока поле owner на текущего авторизованного пользователя."""
+
+        serializer.save(owner=self.request.user)
