a57c47f912fbd678876492e7449ae86120b69142
alexander vagin <sashavagin1987@gmail.com>
2025-11-01 17:37:16 +1000

    - созданы ViewSet-ы для моделей Test, Question, Answer и Generic-и (Create, List, Retrieve, Destroy) для модели TestResult

--- tests/views.py
+++ tests/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView, DestroyAPIView, ListAPIView, RetrieveAPIView
+from rest_framework.viewsets import ViewSet
 
-# Create your views here.
+from tests.models import Test, Question, Answer, TestResult
+from tests.serializer import TestSerializer, QuestionSerializer, AnswerSerializer, TestResultSerializer
+
+
+class TestViewSet(ViewSet):
+    """ViewSet модели Test."""
+
+    queryset = Test.objects.all()
+    serializer_class = TestSerializer
+
+
+class QuestionViewSet(ViewSet):
+    """ViewSet модели Question."""
+
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+
+
+class AnswerViewSet(ViewSet):
+    """ViewSet модели Answer."""
+
+    queryset = Answer.objects.all()
+    serializer_class = AnswerSerializer
+
+
+class TestResultCreateAPIView(CreateAPIView):
+    """Generic создания результата теста."""
+
+    queryset = TestResult.objects.all()
+    serializer_class = TestResultSerializer
+
+
+class TestResultListAPIView(ListAPIView):
+    """Generic списка результата тестов."""
+
+    queryset = TestResult.objects.all()
+    serializer_class = TestResultSerializer
+
+
+class TestResultRetrieveAPIView(RetrieveAPIView):
+    """Generic списка результата тестов."""
+
+    queryset = TestResult.objects.all()
+    serializer_class = TestResultSerializer
+
+
+class TestResultDestroyAPIView(DestroyAPIView):
+    """Generic удаления результата теста."""
+
+    queryset = TestResult.objects.all()
+    serializer_class = TestResultSerializer
