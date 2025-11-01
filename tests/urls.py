d3c3c27fbe0076180b40e1bfce49de495e1404eb
alexander vagin <sashavagin1987@gmail.com>
2025-11-02 05:23:17 +1000

    - убран путь до контроллера TestResultCreateAPIView

--- tests/urls.py
+++ tests/urls.py
@@ -5,7 +5,6 @@ from tests.views import (
     TestViewSet,
     QuestionViewSet,
     AnswerViewSet,
-    TestResultCreateAPIView,
     TestResultDestroyAPIView,
     TestResultListAPIView,
     TestResultRetrieveAPIView,
@@ -19,11 +18,6 @@ router.register("question", QuestionViewSet, basename="question")
 router.register("answer", AnswerViewSet, basename="answer")
 
 urlpatterns = [
-    path(
-        "test/result/create/",
-        TestResultCreateAPIView.as_view(),
-        name="test_result_create",
-    ),
     path("test/result/", TestResultListAPIView.as_view(), name="test_result_list"),
     path(
         "test/result/retrieve/<int:pk>/",
