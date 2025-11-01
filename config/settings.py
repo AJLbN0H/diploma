a530d609478057401dc700df625e0c5d5bd3f118
alexander vagin <sashavagin1987@gmail.com>
2025-11-01 22:37:12 +1000

    - в INSTALLED_APPS добавлено приложение tests и rest_framework_simplejwt, а также настройки для работы библиотеки simplejwt

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
 import os
 from dotenv import load_dotenv
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
     "rest_framework",
     "users",
     "materials",
+    "tests",
+    "rest_framework_simplejwt",
 ]
 
 MIDDLEWARE = [
@@ -132,3 +134,17 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "users.User"
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=180),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+}
