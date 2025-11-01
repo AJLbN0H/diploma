a2ddb62b7fe0c57d5107f7ed40d4da6f59c3e326
alexander vagin <sashavagin1987@gmail.com>
2025-11-02 02:06:14 +1000

    - в perform_create добавлены проверки в какую группу входит пользователь

--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import Group
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import AllowAny
 
@@ -13,8 +14,19 @@ class UserCreateApiView(CreateAPIView):
     permission_classes = (AllowAny,)
 
     def perform_create(self, serializer):
-        """Метод активации пользователя при его создании."""
+        """Метод активацирующий пользователя при его создании и проверяющий в какую группу он входит."""
 
         user = serializer.save(is_active=True)
         user.set_password(user.password)
+
+        if user.role == 'admin':
+            group = Group.objects.get_or_create(name='Администраторы')
+            user.groups.add(group)
+        elif user.role == 'teacher':
+            group = Group.objects.get_or_create(name='Преподователи')
+            user.groups.add(group)
+        elif user.role == 'student':
+            group = Group.objects.get_or_create(name='Студенты')
+            user.groups.add(group)
+
         user.save()
