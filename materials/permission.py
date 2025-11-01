33b2936d30cd140191497eed4890dec58cc137c7
alexander vagin <sashavagin1987@gmail.com>
2025-11-02 04:09:19 +1000

    - права доступа собраны вместе

--- materials/permission.py
+++ materials/permission.py
@@ -29,3 +29,51 @@ class IsOwner(BasePermission):
         if request.user == obj.owner:
             return True
         return False
+
+
+
+class IsAdminOrTeacherOwner(BasePermission):
+
+    def has_permission(self, request, view):
+        if not request.user.is_authenticated:
+            return False
+
+        is_admin = IsAdmin().has_permission(request, view)
+        is_teacher = IsTeacher().has_permission(request, view)
+
+        return is_admin or is_teacher
+
+    def has_object_permission(self, request, view, obj):
+        if IsAdmin().has_permission(request,view):
+            return True
+
+        if IsTeacher().has_permission(request, view):
+            print(IsOwner().has_object_permission(request, view, obj))
+            return IsOwner().has_object_permission(request, view, obj)
+
+        return False
+
+
+class IsAdminOrTeacher(BasePermission):
+
+    def has_permission(self, request, view):
+        if not request.user.is_authenticated:
+            return False
+
+        is_admin = IsAdmin().has_permission(request, view)
+        is_teacher = IsTeacher().has_permission(request, view)
+
+        return is_admin or is_teacher
+
+
+class IsAdminOrTeacherOrStudent(BasePermission):
+
+    def has_permission(self, request, view):
+        if not request.user.is_authenticated:
+            return False
+
+        is_admin = IsAdmin().has_permission(request, view)
+        is_teacher = IsTeacher().has_permission(request, view)
+        is_student = IsStudent().has_permission(request,view)
+
+        return is_admin or is_teacher or is_student
