58150e603a641dfd2dc4f4ee159b008e3d237cb8
alexander vagin <sashavagin1987@gmail.com>
2025-11-03 05:53:08 +1000

    - в моделе Material в поле section добавлен related_name='materials'

--- materials/models.py
+++ materials/models.py
@@ -36,7 +36,7 @@ class Material(models.Model):
         null=True,
     )
     section = models.ForeignKey(
-        Section, verbose_name="Раздел", on_delete=models.CASCADE, blank=True, null=True
+        Section, verbose_name="Раздел", on_delete=models.CASCADE, blank=True, null=True, related_name='materials'
     )
     owner = models.ForeignKey(
         User,
